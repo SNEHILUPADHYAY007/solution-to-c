94651f0b9e3e74f09fbe80b818ee842a44bff044
SNEHILUPADHYAY007 <snehiupadhyay007@gmail.com>
2020-01-02 20:33:20 +0530

    adding solution 3,4

---
+++ chapter 2/solu2.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float c1,c2,div;
+    printf("enter two numbers:");
+    scanf("%f %f",&c1,&c2);
+    div=c1/c2;
+    printf("div=%f number1=%f number2=%f",div,c1,c2);
+
+
+
+
+return 0;
+}
